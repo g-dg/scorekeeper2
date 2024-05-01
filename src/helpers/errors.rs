f5ed14d9d8e3d2f59a82ffa2e7a8cc844498a5fc
Garnet DeGelder <garnet@garnetdg.ca>
2024-05-01 13:58:21 -0400

    Make GenericError implement std::error::Error

--- src/helpers/errors.rs
+++ src/helpers/errors.rs
@@ -1,9 +1,12 @@
+use std::{error::Error, fmt::Display};
+
 use axum::http::StatusCode;
 
 #[derive(Debug)]
 pub struct GenericError(u16);
 
 impl GenericError {
+    pub const GENERIC: GenericError = GenericError(0);
     pub const BAD_REQUEST: GenericError = GenericError(400);
     pub const FORBIDDEN: GenericError = GenericError(403);
     pub const NOT_FOUND: GenericError = GenericError(404);
@@ -12,5 +15,24 @@ impl GenericError {
 
     pub fn to_status_code(&self) -> StatusCode {
         StatusCode::from_u16(self.0).unwrap()
+    }
+}
+
+impl Error for GenericError {}
+
+impl Display for GenericError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self.0 {
+                400 => "Bad Request",
+                403 => "Forbidden",
+                404 => "Not Found",
+                409 => "Conflict",
+                500 => "Internal Server Error",
+                _ => "Unknown Error",
+            }
+        )
     }
 }
